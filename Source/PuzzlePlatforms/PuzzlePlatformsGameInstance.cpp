603961d4bfdf20026336306472b6d5c6573d319a
Preston Grau <preston.grau@gmail.com>
2021-10-19 05:11:52 -0500

    Console Commands

---
+++ Source/PuzzlePlatforms/PuzzlePlatformsGameInstance.cpp
@@ -0,0 +1,31 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "PuzzlePlatformsGameInstance.h"
+#include "Engine/Engine.h"
+
+UPuzzlePlatformsGameInstance::UPuzzlePlatformsGameInstance(const FObjectInitializer& ObjectInitializer)
+{
+	UE_LOG(LogTemp, Warning, TEXT("Constructor"));
+}
+
+void UPuzzlePlatformsGameInstance::Host()
+{
+	UEngine* Engine = GetEngine();
+	if (!ensure(Engine != nullptr)) return;
+
+	Engine->AddOnScreenDebugMessage(0, 2, FColor::Green, TEXT("Hosting"));
+}
+
+void UPuzzlePlatformsGameInstance::Join(const FString& Address)
+{
+	UEngine* Engine = GetEngine();
+	if (!ensure(Engine != nullptr)) return;
+
+	Engine->AddOnScreenDebugMessage(0, 5, FColor::Green, FString::Printf(TEXT("Joining %s"), *Address));
+}
+
+void UPuzzlePlatformsGameInstance::Init()
+{
+	UE_LOG(LogTemp, Warning, TEXT("Init"));
+}
